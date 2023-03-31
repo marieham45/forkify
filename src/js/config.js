05a24a4cb4424cd6eac3a245b1a1ddc7637403dc
marieham45 <marie.hamsikova@centrum.cz>
2023-03-31 17:21:57 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'dc05c2ea-13f8-4766-ac51-17e560413f88';
+export const MODAL_CLOSE_SEC = 2.5;
